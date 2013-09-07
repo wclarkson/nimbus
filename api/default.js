566ec8123f603d170cd91fcb6cc571fc9b5d1729
Ubuntu <ubuntu@ip-10-112-83-26.ec2.internal>
2013-09-07 12:13:43 +0000

    added api routes

--- api/default.js
+++ api/default.js
@@ -2,12 +2,41 @@ var express = require('express');
 var app = express();
 var logger = require('logger').createLogger();
 
+
 app.get('/', function(req, res) {
-  var redis = require('redis');
-  var client = redis.createClient();
-  client.get("greeting", function(err, reply) {
-    res.send(reply);
-  });
+//  var redis = require('redis');
+//  var client = redis.createClient();
+  console.log(req);
+  res.send(req.url);
+//  client.get("greeting", function(err, reply) {
+//    res.send(reply);
+//  });
+});
+
+app.get('/q', function(req, res) {
+  console.log(req.params);
+  res.send('abc');
+});
+
+
+app.post('/auth' function(req, res) {
+
+});
+
+app.delete('/auth' function(req, res) {
+
+});
+
+app.post('/service', function(req, res) {
+
+});
+
+app.delete('/service', function(req, res) {
+
+});
+
+app.post('/signup', function(req, res) {
+
 });
 
 app.listen(3000);
