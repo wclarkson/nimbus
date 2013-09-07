76387df886ca5eff510776da72cdafc53e850530
Ubuntu <ubuntu@ip-10-112-83-26.ec2.internal>
2013-09-07 14:54:14 +0000

    working on backend user authentication

--- api/default.js
+++ api/default.js
@@ -1,29 +1,63 @@
 var express = require('express');
 var app = express();
 var logger = require('logger').createLogger();
+var passport = require('passport')
+var BasicStrategy = require('passport-http').BasicStrategy
+var User = require('passport-http').User
 
-
-app.get('/', function(req, res) {
-//  var redis = require('redis');
-//  var client = redis.createClient();
-  console.log(req);
-  res.send(req.url);
-//  client.get("greeting", function(err, reply) {
-//    res.send(reply);
-//  });
+var auth = express.basicAuth(function(user, pass, callback) {
+  var result = (user === 'test' && pass === 'test');
+  callback(null, result);
 });
 
-app.get('/q', function(req, res) {
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+          done(err, user);
+            });
+});
+
+passport.use(new BasicStrategy(
+      function(username, password, done) {
+        return done(null, true);
+        User.findOne({ username : username }, function (err, user) {
+          if (err) { return done(err); }
+          if (!user) { return done(null, false); }
+          if (!user.validPassword(password)) { return done(null, false); }
+          return done(null, user);
+        });
+        return done(null, null);
+      }));
+
+app.get('/', function(req, res) {
+  //  var redis = require('redis');
+  //  var client = redis.createClient();
+  console.log(req);
+  res.send(req.url);
+  //  client.get("greeting", function(err, reply) {
+  //    res.send(reply);
+  //  });
+});
+
+app.get('/q', auth, function(req, res) {
   console.log(req.params);
   res.send('abc');
 });
 
+app.get('/auth', 
+    passport.authenticate('basic', { session : true }),
+    function(req, res) {
+      res.send('you\'re logged in!');
+    }
+    );
 
-app.post('/auth' function(req, res) {
-
-});
-
-app.delete('/auth' function(req, res) {
+app.delete('/auth', function(req, res) {
 
 });
 
